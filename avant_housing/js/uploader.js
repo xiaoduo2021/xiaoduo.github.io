3d7864a62cd0861a70469b4477fcd5407cadb393
Khalil <lvyuan95@qq.com>
2017-06-07 01:44:57 -0400

    add

--- avant_housing/js/uploader.js
+++ avant_housing/js/uploader.js
@@ -3,6 +3,7 @@ var images = [];
 var wechatImage;
 
 $(function () {
+
     $('#loadingToast').hide();
     // 允许上传的图片类型
     var allowTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'];
@@ -13,10 +14,11 @@ $(function () {
     // 最大上传图片数量
     var maxCount = 6;
     $('#uploaderInput').on('change', function (event) {
-
+        $('#loadingToast').show();
         var files = event.target.files;
         // 如果没有选中文件，直接返回
         if (files.length === 0) {
+            $('#loadingToast').hide();
             return;
         }
 
@@ -26,17 +28,18 @@ $(function () {
 
             // 如果类型不在允许的类型范围内
             if (allowTypes.indexOf(file.type) === -1) {
-                $.weui.alert('该类型不允许上传');
+                weui.alert('该类型不允许上传');
                 continue;
             }
 
             if (file.size > maxSize) {
-                $.weui.alert('图片太大，不允许上传');
+                weui.alert('图片太大，不允许上传');
                 continue;
             }
 
             if ($('#property_images', '.weui-uploader__file').length >= maxCount) {
-                $.weui.alert('最多只能上传' + maxCount + '张图片');
+                weui.alert('最多只能上传' + maxCount + '张图片');
+                $('#loadingToast').hide();
                 return;
             }
 
@@ -56,11 +59,17 @@ $(function () {
                     var base64 = canvas.toDataURL('image/png');
 
                     // 插入到预览区
-                    var $preview = $('<li class="weui-uploader__file" style="background-image:url(' + base64 + ')"><div class="weui-uploader__file-content">0%</div></li>');
-                    $('#weui-uploader__files').append($preview);
-                    var num = $('.weui-uploader__file').length;
-                    $('.weui-uploader__info').html(num + '/' + maxCount);
-                    images.push(base64);
+                    if ($('.weui-uploader__file').length + 1 <= maxCount){
+                        var $preview = $('<li class="weui-uploader__file" style="background-image:url(' + base64 + ')"><div class="weui-uploader__file-content">0%</div></li>');
+                        $('#weui-uploader__files').append($preview);
+                        var num = $('.weui-uploader__file').length;
+                        $('.weui-uploader__info').html(num + '/' + maxCount);
+                        images.push(base64);
+                    }
+                    else{
+                        weui.alert('最多只能上传' + maxCount + '张图片');
+                    }
+                    $('#loadingToast').hide();
                 };
 
                 img.src = e.target.result;
@@ -93,11 +102,16 @@ function postData() {
             'des': des,
             'images': JSON.stringify(images)}
     }).done(function( o ) {
-        $('#loadingToast').hide();
-        $(".form").hide();
-        $("#hd2").show();
-        $("#bd2").show();
-        $('#bd2').html('<img class="result_img" src="data:image/png;base64, ' + o.data.slice(2,-1) +  ' " alt="Red dot" />')
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-    });
+        if (o && o.status == 0){
+            $('#loadingToast').hide();
+            $(".form").hide();
+            $("#hd2").show();
+            $("#bd2").show();
+            $('#bd2').html('<img class="result_img" src="data:image/png;base64, ' + o.data.slice(2,-1) +  ' " alt="Red dot" />')
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+        }
+
+    }).fail(function() {
+        weui.alert('提交失败，请重试')
+  });
 }
