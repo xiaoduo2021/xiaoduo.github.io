6f282b098dd612790cc2978086c9b1e02a6de9fc
Khalil <lvyuan95@qq.com>
2018-06-29 23:12:16 -0400

    click image

--- koubei/ad.js
+++ koubei/ad.js
@@ -1,25 +1,66 @@
+const adData = {
+  footer: {
+    UTM: "AvantClass",
+    UTSG: "SavvyPro",
+    UTSC: "YouClass",
+  },
+  ad: {
+    UTM: [
+      {
+        img: "https://www.joc.com/sites/default/files/field_feature_image/KFC_0.png",
+        url: "http://www.kfc.ca",
+      },
+      {
+        img: "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Ftimedotcom.files.wordpress.com%2F2018%2F06%2F800-21.jpg&w=800&q=85",
+        url: "http://www.kfc.ca",
+      },
+    ],
+    UTSG: [
+      {
+        img: "https://www.joc.com/sites/default/files/field_feature_image/KFC_0.png",
+        url: "http://www.kfc.ca",
+      },
+      {
+        img: "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Ftimedotcom.files.wordpress.com%2F2018%2F06%2F800-21.jpg&w=800&q=85",
+        url: "http://www.kfc.ca",
+      },
+    ],
+    UTSC: [
+      {
+        img: "https://www.joc.com/sites/default/files/field_feature_image/KFC_0.png",
+        url: "http://www.kfc.ca",
+      },
+      {
+        img: "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Ftimedotcom.files.wordpress.com%2F2018%2F06%2F800-21.jpg&w=800&q=85",
+        url: "http://www.kfc.ca",
+      },
+    ],
+  },
+}
+
 $(document).ready( function () {
   let campus = getUrlParams('campus').toUpperCase();
   if (!campus){
     return;
   }
 
-  $.getJSON("ad_data.json", function(json) {
-      const adData = json;
-      let ads = adData.ad[campus];
+  let ads = adData.ad[campus];
 
-      // pick random at top
-      const index = Math.floor(Math.random()*ads.length);
-      let ad1 = ads[index];
-      delete ads[ index ];
-      let ad2 = ads[Math.floor(Math.random()*ads.length)];
+  // pick random at top
+  const index = Math.floor(Math.random()*ads.length);
+  let ad1 = ads[index];
+  delete ads[ index ];
+  let ad2 = ads[Math.floor(Math.random()*ads.length)];
 
-      $("#footer_partner").html(adData.footer[campus]);
-      $("#ad1").attr("src", ad1.img);
-      $("#ad2").attr("src", ad2.img);
+  $("#footer_partner").html(adData.footer[campus]);
+  $("#ad1").attr("src", ad1.img);
+  $("#ad2").attr("src", ad2.img);
 
-      $("#ad1").attr("onclick", 'window.open("' + ad1.url + '")');
-      $("#ad2").attr("onclick", 'window.open("' + ad2.url + '")');
+  $("#ad1").click(function(){
+    location.href = ad1.url;
+  });
+  $("#ad2").click(function(){
+    location.href = ad2.url;
   });
 });
 
